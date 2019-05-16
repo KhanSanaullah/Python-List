67d400ae56c4606dec082ed38c746c7b3ef855e8
khan6661 <sanaullahkhan6661@gmail.com>
2019-05-17 03:38:16 +0500

    Updated

--- Guess-The-No.py
+++ Guess-The-No.py
@@ -5,8 +5,7 @@ hidden = random.randrange(1, 6)
 guess = int(input("Please enter your guess:"))
 
 if guess == hidden:
-    print( "Hit!")
-
+    print( "You guess right!")
 elif guess < hidden:
     print("Your guess is too low")
 
